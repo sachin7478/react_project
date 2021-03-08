4dd68e6f990f1d7100641b06b4d8ed7f9425a32e
vmdev <sudmale@xento.com>
2021-03-08 09:56:04 +0530

    Task ID: 12 Revert "to do list program"
    This reverts commit 23a6d41c48d4bea5e9ad0defda1554eb6089547c.

---
+++ src/Todo_list.jsx
@@ -0,0 +1,13 @@
+import react from 'react';
+const Todo_list = (prop) => {
+    return(<><div className="todo_style">
+                <i className="fa fa-times" aria-hidden="true" name={prop.index} onClick={()=>{
+                    prop.onSelect(prop.id);
+                }}/>
+                <li value={prop.index}>{prop.itemVal}
+             </li>
+            </div>
+            
+        </>)
+}
+export default Todo_list;
