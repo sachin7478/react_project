7e7d87a360001b0ac4d84f10da67a690d0756bb9
vmdev <sudmale@xento.com>
2021-03-09 09:08:22 +0530

    normal update

---
+++ google keep ui interface/Keep_App.jsx
@@ -0,0 +1,33 @@
+import react, { useState } from 'react';
+import Header from './Header';
+import CreateNote from './CreateNote';
+import Footer from './Footer';
+import Note from './Note';
+export default function Keep_App() {
+    const [noteList,updateNoteList] = useState([]);
+    const addNote = ( note ) => { 
+        updateNoteList( ( prevNotes ) => {
+            return[ ...prevNotes,  note ];
+        } )
+    }
+    const deleteNote = ( id ) => {
+        updateNoteList( function( OldNotes ) {
+               return OldNotes.filter( function ( currentNote, index ) {
+                return index != id;
+            } )
+         } )
+    }
+    
+    return(
+        <>
+            <Header />
+            <CreateNote passNote={addNote}/>
+            <div className="NoteContainer">
+                { noteList.map( ( currentNote, index ) => {
+                    return ( <Note key={index} id={index} noteData = {currentNote} triggerDeleteNote={deleteNote}/> );
+                } ) }
+            </div>
+            <Footer />
+        </>
+    )
+}
